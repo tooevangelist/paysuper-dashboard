3f2ff3e0f0fce1a721b0e6fe9a2f4db2ff769b7b
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2018-11-01 12:49:17 +0300

    little changes by index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,31 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        ponepay.frontend
-      </h1>
-      <h2 class="subtitle">
-        PONE payment solution customer control panel
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="d-flex align-items-center justify-content-center container">
+                <nuxt-link type="button" class="btn btn-success align-middle" to="/login" active-class="" tag="button">
+                    Sign In
+                </nuxt-link>
+                <nuxt-link type="button" class="btn btn-primary" to="/register" active-class="" tag="button">
+                    Sign Up
+                </nuxt-link>
+            </div>
+        </div>
     </div>
-  </section>
 </template>
 
 <script>
-    import AppLogo from '~/components/AppLogo.vue'
-
     export default {
-        components: { AppLogo },
         layout: 'login'
     }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss" scoped>
+    .container {
+        height: 100vh;
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+        button {
+            margin-right: 7px;
+        }
+    }
 </style>
 
