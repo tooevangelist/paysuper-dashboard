999feeffd0caa6b6dc2d92055ce06a051e1802c6
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2018-11-02 14:49:19 +0300

    bug fixes

--- api/auth.js
+++ api/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     login: function (data) {
-        return axios.post(`${process.env.authServerUrl}/user/login/`, data);
+        return axios.post(`${process.env.authServerUrl}/user/login`, data);
     },
     register: function (data) {
         return axios.post(`${process.env.authServerUrl}/user/create/`, data);
