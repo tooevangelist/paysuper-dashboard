1a9be8b6a258ed57c37b9ad0353a35a687a258f6
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-01-15 10:26:50 +0300

    PAY-80 test page for redirect after finishing a payment

---
+++ pages/payment_finished.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="display: flex; height: 100vh;">
+        <h2 style="margin: auto;">Payment is finished</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: 'login',
+        head: {
+            title: 'Payment is finished'
+        }
+    }
+</script>
