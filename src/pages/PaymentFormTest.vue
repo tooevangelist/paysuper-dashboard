d51078483612d654c17c10222fadb6f5c9da8ee2
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-06-19 14:27:21 +0700

    adds test payform init options

--- src/pages/PaymentFormTest.vue
+++ src/pages/PaymentFormTest.vue
@@ -10,13 +10,25 @@ import axios from 'axios';
 export default {
   async mounted() {
     let result;
+    let request = {
+      project: '5cd5620f06ae110001509185',
+      products: ['5cda8d3938e0e2000176988b', '5cda8e6738e0e2000176988c'],
+    };
+    if (this.$route.query.productsCount) {
+      request.products = request.products.slice(0, this.$route.query.productsCount);
+    }
+    if (this.$route.query.pay) {
+      const [amount, currency] = this.$route.query.pay.split('-');
+      request = {
+        project: '5cda856f38e0e20001769886',
+        amount: Number(amount),
+        currency,
+      };
+    }
     try {
       const { data } = await axios.post(
         'https://p1payapi.tst.protocol.one/api/v1/order',
-        {
-          project: '5cd5620f06ae110001509185',
-          products: ['5cda8d3938e0e2000176988b'],
-        },
+        request,
       );
       result = data;
     } catch (error) {
