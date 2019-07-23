e40fd9eb8bde1561eeeb491e6de9c1152935b891
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-07-23 11:04:48 +0300

    You did great job page

---
+++ src/components/YouDidGreatJob.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: 'YouDidGreatJob',
+};
+</script>
+
+<template>
+<div class="you-did-great-job">
+  <IconCupAndPeople class="icon" />
+  <UiHeader
+    align="center"
+    :hasMargin="true"
+    level="2"
+  >You did a great job!</UiHeader>
+  <p class="text">
+    Now we need a little bit more of your time to ask you a few<br>
+    questions. It will greatly help us to improve your experience<br>
+    from using our service.
+  </p>
+  <UiButton>CONTINUE</UiButton>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.you-did-great-job {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.icon {
+  margin-bottom: 24px;
+}
+
+.text {
+  text-align: center;
+  margin-bottom: 48px;
+}
+</style>
---
+++ src/pages/SignUp.vue
@@ -0,0 +1,18 @@
+
+<script>
+import YouDidGreatJob from '@/components/YouDidGreatJob.vue';
+
+export default {
+  name: 'SignUp',
+
+  components: {
+    YouDidGreatJob,
+  },
+};
+</script>
+
+<template>
+<div class="sign-up">
+  <YouDidGreatJob />
+</div>
+</template>
