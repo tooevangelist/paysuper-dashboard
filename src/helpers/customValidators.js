5fc800654c8b67cfd34a4c8778c0332a1dafed9d
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-08-01 11:43:52 +0300

    fixing validation at profile fields

--- src/helpers/customValidators.js
+++ src/helpers/customValidators.js
@@ -2,9 +2,12 @@ export function onlyRusAndLat(value) {
   if (!value) {
     return true;
   }
-  return /^[A-Za-zА-Яа-я0-9- ]*$/.test(value);
+  return /^[A-Za-zА-Яа-я- ]*$/.test(value);
 }
 
-export function wtf() {
-  return '';
+export function onlyRusAndLatAndNum(value) {
+  if (!value) {
+    return true;
+  }
+  return /^[A-Za-zА-Яа-я0-9- ]*$/.test(value);
 }
