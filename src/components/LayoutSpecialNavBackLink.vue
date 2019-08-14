c68c1622d601fca87e20ec09ab60044eee0f205c
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-08-14 18:05:20 +0300

    changes layout to be customisable, adds ProjectCard sidebar

---
+++ src/components/LayoutSpecialNavBackLink.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  name: 'LayoutSpecialNavBackLink',
+
+  props: {
+    link: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<template>
+<RouterLink
+  class="layout-special-nav-back-link"
+  :to="link.url"
+>
+  <IconArrowDown
+    class="icon"
+    fill="#3d7bf5"
+    width="12"
+    height="8"
+  />
+  <span class="text">
+    {{link.label}}
+
+  </span>
+</RouterLink>
+</template>
+
+<style lang="scss" scoped>
+.layout-special-nav-back-link {
+  display: flex;
+  align-items: center;
+  padding-left: 26px;
+  color: #3d7bf5;
+  font-size: 14px;
+  line-height: 20px;
+}
+
+.icon {
+  transform: rotate(90deg);
+  margin-right: 21px;
+}
+
+.text {
+  margin-top: 1px;
+}
+</style>
---
+++ src/components/LayoutTopControlsDatepicker.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'LayoutTopControlsDatepicker',
+
+  data() {
+    return {
+      innerPeriod: [1534784400000, 1537462800000],
+    };
+  },
+};
+</script>
+
+<template>
+<div class="layout-top-controls-datepicker">
+  <v-datepicker
+    v-model="innerPeriod"
+    class="datepicker"
+    format="MMM DD YYYY"
+    lang="en"
+    rangeSeparator="·"
+    valueType="timestamp"
+    :editable="false"
+    :range="true"
+    @input="$emit('periodChange', innerPeriod)"
+  />
+</div>
+</template>
+
+<style lang="scss" scoped>
+.layout-top-controls-datepicker {
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+}
+.datepicker {
+  max-width: 232px;
+  margin-right: 16px;
+  margin-left: 12px;
+}
+</style>
