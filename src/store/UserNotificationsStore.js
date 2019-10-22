41c3ea777420f4ee0d29ad1507e66695184f3ed5
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-10-22 12:37:19 +0300

    fix notifications fetch

--- src/store/UserNotificationsStore.js
+++ src/store/UserNotificationsStore.js
@@ -28,7 +28,7 @@ export default function createUserNotificationsStore() {
           const { data } = await axios.get(
             `{apiUrl}/admin/api/v1/merchants/${id}/notifications?sort[]=-created_at`,
           );
-          commit('notifications', data.items);
+          commit('notifications', data.items || []);
         } catch (error) {
           console.error(error);
         }
