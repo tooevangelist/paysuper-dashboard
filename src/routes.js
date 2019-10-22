8c9fff1ad237327071c38ff7d8d3022106fa497d
Andrey Solodovnikov <a.solodovnikov@office.ngs.ru>
2019-10-22 11:23:38 +0300

    routes fixes for PaymentFormSdk

--- src/routes.js
+++ src/routes.js
@@ -230,19 +230,24 @@ const routes = [
     name: 'Login',
   },
   {
-    path: '/payform-sdk/',
+    path: '/form-demo/',
     component: () => import('@/pages/PaymentFormSdk.vue'),
-    meta: { layout: 'PageShallow', isStoreInitDisabled: true },
+    meta: { layout: 'PageShallow', initStore: false },
+    name: 'PaymentFormSdk',
+  },
+  {
+    path: '/payform-sdk/',
+    redirect: { name: 'PaymentFormSdk' },
   },
   {
     path: '/payform-page/',
     component: () => import('@/pages/PaymentFormPage.vue'),
-    meta: { layout: 'PageFormLayout', isStoreInitDisabled: true },
+    meta: { layout: 'PageFormLayout', initStore: false },
   },
   {
     path: '/payform-loading/',
     component: () => import('@/pages/PaymentFormLoading.vue'),
-    meta: { layout: 'PageFormLayout', isStoreInitDisabled: true },
+    meta: { layout: 'PageFormLayout', initStore: false },
   },
   {
     path: '/profile/',
