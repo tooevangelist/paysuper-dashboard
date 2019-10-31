932009849d465ca881e7d86d2ef6fe6c0bf920bb
Aleksandr Grebnev <grebnev.av@gmail.com>
2019-10-31 12:23:36 +0400

    menu header, #192443

---
+++ src/components/IconCompany.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <!-- eslint-disable -->
+    <path d="M7.6 0L0 4V5.6H15.2V4L7.6 0ZM11.2 7.2V12.8H13.6V7.2H11.2ZM0 16.8H15.2V14.4H0V16.8ZM6.4 7.2V12.8H8.8V7.2H6.4ZM1.6 7.2V12.8H4V7.2H1.6Z" fill="#78909C"/>
+  </svg>
+</template>
