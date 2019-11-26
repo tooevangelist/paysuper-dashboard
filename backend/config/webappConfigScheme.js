e72ccb51ca34643a681a1dae84471fa58328e17b
Pavel Kokovin <pavel.kokovin@protocol.one>
2019-11-26 22:53:46 +0700

    Add env

--- backend/config/webappConfigScheme.js
+++ backend/config/webappConfigScheme.js
@@ -7,6 +7,10 @@ module.exports = {
     name: 'VUE_APP_P1PAYAPI_URL',
     default: 'https://p1payapi.tst.protocol.one',
   },
+  hellosignClientId: {
+    name: 'HELLOSIGN_CLIENT_ID',
+    default: '2599245f066de53be8e5837360edf3ac',
+  },
   websocketUrl: {
     name: 'VUE_APP_WEBSOCKET_URL',
     default: 'wss://cf.tst.protocol.one/connection/websocket',
